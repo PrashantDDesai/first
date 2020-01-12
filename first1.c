eed1bb7bf2a6ddc330d3e3560bc0c4defd8c0546
PrashantDDesai <59794627+PrashantDDesai@users.noreply.github.com>
2020-01-12 18:30:36 +0530

    Update first1.c

--- first1.c
+++ first1.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+int mystrcmp(connst *char, const *char)
 void main()
 {
-  char *str1,*str2;
+  char str1[100],str2[100];
   printf("Enter string 1: \n");
   gets(str1);
   printf("Enter string 2: \n");
@@ -11,4 +12,19 @@ void main()
   printf("\n");
   puts(str2);
   return 0;
+}
+
+int mystrcmp(const char *a, const char *b)
+{
+  while(*a==*b && *a!='\0' && *b!='\0')
+  {
+    a++;
+    b++;
+  }
+  if(a==b)
+  {
+    return 0;
+  }
+  else
+    return *a-*b;
 }
